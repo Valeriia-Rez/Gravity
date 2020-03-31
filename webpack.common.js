3fddba5009e44f7994a87fc0b232019c1b65ffbb
Valeriia-Rez <Valeria09122016@gmail.com>
2020-03-31 13:34:19 +0300

    Add docs

--- webpack.common.js
+++ webpack.common.js
@@ -9,7 +9,7 @@ module.exports = {
     entry: './src/js/index.js',
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
     },
     module: {
         rules: [
@@ -29,8 +29,7 @@ module.exports = {
             //Sass Loaders
             {
                 test: /\.(sa|sc|c)ss$/,
-                use: [
-                    {
+                use: [{
                         loader: MiniCssExtractPlugin.loader,
                         options: {
                             // only enable hot in development
@@ -43,7 +42,7 @@ module.exports = {
                     {
                         loader: 'postcss-loader',
                         options: {
-                            plugins: function () {
+                            plugins: function() {
                                 return [
                                     require('precss'),
                                     require('autoprefixer')
@@ -56,14 +55,12 @@ module.exports = {
             },
             {
                 test: /\.(png|jpe?g|gif|svg)$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            outputPath: 'images'
-                        }
+                use: [{
+                    loader: "file-loader",
+                    options: {
+                        outputPath: 'images'
                     }
-                ]
+                }]
             }
         ]
     },
